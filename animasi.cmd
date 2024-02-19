812e582970ba746d304861ec2760e4bbda2cfd51
Program Is Fun <134474566+26ProgramIsFun@users.noreply.github.com>
2024-02-19 15:15:52 +0700

    upload

---
+++ animasi.cmd
@@ -0,0 +1,23 @@
+@echo off
+cls
+color a
+echo Welcome to ASCII Animation!
+timeout /t 2 > nul
+
+:loop
+cls
+echo /\_/\  
+echo( ' ' ) 
+echo(  -  )
+timeout /t 1 > nul
+cls
+echo /\_/\  
+echo( o o ) 
+echo(  -  )
+timeout /t 1 > nul
+cls
+echo /\_/\  
+echo( - - ) 
+echo(  -  )
+timeout /t 1 > nul
+goto loop
